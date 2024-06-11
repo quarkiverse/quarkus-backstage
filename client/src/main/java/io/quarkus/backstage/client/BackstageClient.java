73539ed0b70e1dfb79ea3cb08d4414df395b61b5
Ioannis Canellos <iocanel@gmail.com>
2024-06-11 10:36:34 +0300

    feat: Add rest client

---
+++ client/src/main/java/io/quarkus/backstage/client/BackstageClient.java
@@ -0,0 +1,37 @@
+package io.quarkiverse.backstage.client;
+
+import java.util.Set;
+
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import io.quarkiverse.backstage.model.Entity;
+
+@RegisterRestClient(configKey = "quarkus.backstage")
+public interface BackstageClient {
+
+    @GET
+    @Path("/entities")
+    Set<Entity> getAllEntities();
+
+    @GET
+    @Path("/entities/by-uid/{uid}")
+    Entity getEntity(String uid);
+
+    @GET
+    @Path("/entities/by-name/{kind}/{namespace}/{name}")
+    Entity getEntities(String kind, String namespace, String name);
+
+    @DELETE
+    @Path("/entities/by-uid/{uid}")
+    void deleteEntity(String uid);
+
+    @POST
+    @Path("/entities")
+    Set<Entity> createEntities(Set<Entity> entities);
+
+}
