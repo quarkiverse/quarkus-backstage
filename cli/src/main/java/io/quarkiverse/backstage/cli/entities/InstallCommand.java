036b1e5eab14f2f3b6d8f3f44c978a2676d5c6cb
Ioannis Canellos <iocanel@gmail.com>
2024-10-10 17:56:02 +0300

    chore: align entities install with template install

--- cli/src/main/java/io/quarkiverse/backstage/cli/entities/InstallCommand.java
+++ cli/src/main/java/io/quarkiverse/backstage/cli/entities/InstallCommand.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 
 import io.quarkiverse.backstage.cli.common.GenerationBaseCommand;
 import io.quarkiverse.backstage.cli.utils.Git;
-import io.quarkiverse.backstage.cli.utils.Github;
 import io.quarkiverse.backstage.deployment.utils.Serialization;
 import io.quarkiverse.backstage.rest.CreateLocationRequest;
 import io.quarkiverse.backstage.rest.RefreshEntity;
@@ -15,6 +14,8 @@ import io.quarkiverse.backstage.runtime.BackstageClient;
 import io.quarkiverse.backstage.v1alpha1.Entity;
 import io.quarkiverse.backstage.v1alpha1.EntityList;
 import io.quarkiverse.backstage.v1alpha1.Location;
+import io.quarkus.devtools.project.QuarkusProject;
+import io.quarkus.devtools.project.QuarkusProjectHelper;
 import io.quarkus.devtools.utils.Prompt;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -22,8 +23,11 @@ import picocli.CommandLine.Option;
 @Command(name = "install", sortOptions = false, mixinStandardHelpOptions = false, header = "Install Backstage Entities.", headerHeading = "%n", commandListHeading = "%nCommands:%n", synopsisHeading = "%nUsage: ", optionListHeading = "%nOptions:%n")
 public class InstallCommand extends GenerationBaseCommand {
 
-    @Option(names = { "--github-token" }, description = "The github token to use for publishing components to gist.")
-    Optional<String> githubToken = Optional.empty();
+    @Option(names = { "-r", "--remote" }, description = "The git remote to push the template to.")
+    String remote = "origin";
+
+    @Option(names = { "-b", "--branch" }, description = "The git branch to push the template to.")
+    String branch = "backstage";
 
     public InstallCommand(BackstageClient backstageClient) {
         super(backstageClient);
@@ -36,19 +40,21 @@ public class InstallCommand extends GenerationBaseCommand {
             return;
         }
 
+        final QuarkusProject project = QuarkusProjectHelper.getProject(getWorkingDirectory());
         List<EntityListItem> items = new ArrayList<>();
 
         String content = Serialization.asYaml(entityList);
         Path catalogPath = getWorkingDirectory().resolve("catalog-info.yaml");
         writeStringSafe(catalogPath, content);
 
-        Optional<String> url = Optional.empty();
-        Optional<String> githubToken = Github.getToken();
+        Optional<String> url = Git.getUrl(remote, branch, project.getProjectDirPath().relativize(catalogPath));
+        if (url.isEmpty()) {
+            System.out.println("No git remote url found. Template cannot be published. Aborting.");
+            return;
+        }
 
-        if (githubToken.isPresent()) {
-            url = Github.createOrUpdatePrivateGist(githubToken, "Backstage entities", "catalog-info.yaml", content);
-        } else if (Prompt.yesOrNo(false, "This operation will trigger a git commit and push. Would you like to proceed? ")
-                && commit() && push()) {
+        if (Prompt.yesOrNo(false, "This operation will trigger a git commit and push. Would you like to proceed? ")
+                && commit(catalogPath) && push()) {
             System.out.println("Backstage entities pushed to the remote repository.");
         } else {
             System.out.println("Backstage entities not pushed to the remote repository. Aborting.");
@@ -91,13 +97,12 @@ public class InstallCommand extends GenerationBaseCommand {
         System.out.println(table.getContent());
     }
 
-    private boolean commit() {
-        Path catalogPath = getWorkingDirectory().resolve("catalog-info.yaml");
-        return Git.commit("Backstage entities generated.", catalogPath);
+    private boolean commit(Path... paths) {
+        return Git.commit("Backstage entities generated.", paths);
     }
 
     private boolean push() {
-        Git.push("origin", "main");
+        Git.push(remote, branch);
         return true;
     }
 }
