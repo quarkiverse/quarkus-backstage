e8009d22105db6cab9ee132a6e4316ac8d9e1195
Ioannis Canellos <iocanel@gmail.com>
2024-11-04 11:22:14 +0200

    test: that templates are automatically installed

--- integration-tests/src/test/java/io/quarkiverse/backstage/it/BackstageDevServiceTest.java
+++ integration-tests/src/test/java/io/quarkiverse/backstage/it/BackstageDevServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import jakarta.inject.Inject;
 
@@ -12,6 +13,7 @@ import org.junit.jupiter.api.Test;
 
 import io.quarkiverse.backstage.client.BackstageClient;
 import io.quarkiverse.backstage.client.model.LocationEntry;
+import io.quarkiverse.backstage.scaffolder.v1beta3.Template;
 import io.quarkiverse.backstage.v1alpha1.Entity;
 import io.quarkus.test.junit.QuarkusTest;
 
@@ -46,7 +48,7 @@ public class BackstageDevServiceTest {
         assertTrue(entities.stream().anyMatch(e -> e.getKind().equalsIgnoreCase("component")));
     }
 
-    @Test
+   @Test
     public void shouldFindComponentLocation() {
         List<Entity> entities = backstageClient.entities().list();
         Optional<Entity> entitty = entities.stream().filter(e -> e.getKind().equalsIgnoreCase("component")).findFirst();
@@ -60,5 +62,15 @@ public class BackstageDevServiceTest {
 
             assertNotNull(location);
         });
+    }
+
+    @Test
+    public void shouldFindTemplates() {
+        List<Entity> entities = backstageClient.entities().list();
+        List<Template> templates = entities.stream()
+                .filter(e -> e instanceof Template)
+                .map(e -> (Template) e)
+                .collect(Collectors.toList());
+        assertTrue(templates.size() >= 2);
     }
 }
