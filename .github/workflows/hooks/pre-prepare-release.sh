0c57abd800febc3a5c40fa022941c966a4dd6ddf
George Gastaldi <gegastaldi@gmail.com>
2024-11-11 15:38:57 -0300

    Create pre-prepare-release.sh
    This script is called during the release process.
    
    - Needed for #51

---
+++ .github/workflows/hooks/pre-prepare-release.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#-------------------------------------
+# Hook invoked by the release workflow
+#-------------------------------------
+
+# Uncomment the following line to generate the screencasts
+#./docs/modules/ROOT/assets/generate-screencasts
