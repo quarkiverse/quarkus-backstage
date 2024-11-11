a1388f9932e6d04bd172a0f9dc8bb134f85db823
George Gastaldi <gegastaldi@gmail.com>
2024-11-11 15:55:52 -0300

    Install required packages

--- .github/workflows/hooks/pre-prepare-release.sh
+++ .github/workflows/hooks/pre-prepare-release.sh
@@ -3,5 +3,17 @@
 # Hook invoked by the release workflow
 #-------------------------------------
 
+# Update the package list
+sudo apt-get update
+
+# Install Asciicinema
+sudo apt-get install asciinema
+
+# Install Asciinema-automation
+python3 -m pip install asciinema-automation
+
+# Install Asciinema-Agg
+cargo install --git https://github.com/asciinema/agg
+
 # Uncomment the following line to generate the screencasts
 #./docs/modules/ROOT/assets/generate-screencasts
