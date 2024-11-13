78875afd95f8a773d3c56c216cf558d72c8727cc
George Gastaldi <gegastaldi@gmail.com>
2024-11-13 12:16:43 -0300

    Update pre-prepare-release.sh

--- .github/workflows/hooks/pre-prepare-release.sh
+++ .github/workflows/hooks/pre-prepare-release.sh
@@ -16,7 +16,9 @@ python3 -m pip install asciinema-automation
 cargo install --git https://github.com/asciinema/agg
 
 # Generate screencasts
-pushd docs/modules/ROOT/assets/ && ./generate-screencasts && popd
+cd docs/modules/ROOT/assets/ 
+./generate-screencasts 
+cd ../../../../
 
 # Commit changes 
 git commit -m "Update screencasts for ${CURRENT_VERSION}" -a
