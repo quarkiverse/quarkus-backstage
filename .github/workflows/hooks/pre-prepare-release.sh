72b6dd96254ea553ccf156d29cefdd06a2bdf5be
George Gastaldi <gegastaldi@gmail.com>
2024-11-13 17:13:25 -0300

    Using SDKMan

--- .github/workflows/hooks/pre-prepare-release.sh
+++ .github/workflows/hooks/pre-prepare-release.sh
@@ -16,13 +16,16 @@ python3 -m pip install asciinema-automation
 cargo install --git https://github.com/asciinema/agg
 
 # Install Quarkus CLI
-curl -Ls https://sh.jbang.dev | bash -s - trust add https://repo1.maven.org/maven2/io/quarkus/quarkus-cli/
-curl -Ls https://sh.jbang.dev | bash -s - app install --fresh --force quarkus@quarkusio
+export SDKMAN_DIR="$HOME/.sdkman" && curl -s "https://get.sdkman.io?rcupdate=false" | bash
+source $HOME/.sdkman/bin/sdkman-init.sh && sdk env install
+sdk version
+sdk install quarkus
+quarkus version 
 
 # Generate screencasts
-#cd docs/modules/ROOT/assets/ 
-#./generate-screencasts 
-#cd ../../../../
+cd docs/modules/ROOT/assets/ 
+./generate-screencasts 
+cd ../../../../
 
 # Commit changes 
-#git commit -m "Update screencasts for ${CURRENT_VERSION}" -a
+git commit -m "Update screencasts for ${CURRENT_VERSION}" -a
