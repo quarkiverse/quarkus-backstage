73f3d3a4cb82cbabc0781eee8091f6cb74da7f70
Ioannis Canellos <iocanel@gmail.com>
2024-11-14 11:51:24 +0200

    feat: install jbang in pre-prepare-release.sh

--- .github/workflows/hooks/pre-prepare-release.sh
+++ .github/workflows/hooks/pre-prepare-release.sh
@@ -26,7 +26,18 @@ echo "#!/bin/sh" > ~/.local/bin/quarkus
 echo "java -jar ${HOME}/tools/quarkus-cli.jar \$*" >> ~/.local/bin/quarkus
 chmod +x ~/.local/bin/quarkus
 quarkus version
-echo $PATH
+
+# Install JBang
+JBANG_VERSION="0.119.0"
+JBANG_CLI_URL="https://repo1.maven.org/maven2/dev/jbang/jbang-cli/${JBANG_VERSION}/jbang-cli-${JBANG_VERSION}-all.jar"
+echo "Downloading JBang CLI from ${JBANG_CLI_URL}"
+mkdir ~/tools
+curl -Ls ${JBANG_CLI_URL} -o ~/tools/jbang-all.jar
+mkdir ~/.local/bin -p
+echo "#!/bin/sh" > ~/.local/bin/jbang
+echo "java -jar ${HOME}/tools/jbang-all.jar \$*" >> ~/.local/bin/jbang
+chmod +x ~/.local/bin/jbang
+jbang version
 
 # Generate screencasts
 cd docs/modules/ROOT/assets/ 
